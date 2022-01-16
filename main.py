ccb5a7e2d0d20125dbee773a1c38a03c00e4d57d
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-01-16 14:08:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Given n as input. Print following:
+ 'Hello' if n is divisible by 5
+ 'Python' otherwise
+"""
+
+n = int(input("Enter Number: "))
+num = 5
+if (n%num==0):
+  print("Hello")
+else:
+  print("Python")
